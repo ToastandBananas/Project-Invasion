115ee01cda2b351552b387249ee4d81166f04446
Jay Cuellar <cuellar.jay@gmail.com>
2020-11-08 18:02:35 -0800

    Attacker movement and start defender

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    float currentSpeed = 1f;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
