a784479188567c19fb08a771f18613d183320c8e
Jay Cuellar <cuellar.jay@gmail.com>
2020-11-11 23:22:16 -0800

    Defender buttons started

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DefenderButton : MonoBehaviour
+{
+    List<DefenderButton> buttons = new List<DefenderButton>();
+
+    void Start()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+    }
+
+    public void SelectDefender()
+    {
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(61, 61, 61, 255);
+        }
+
+        Debug.Log("You clicked on " + transform.parent.name);
+    }
+}
