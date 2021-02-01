b46567ed08314e0d6b51fc25286daab83c36b2b7
Jay Cuellar <cuellar.jay@gmail.com>
2021-01-31 18:35:47 -0800

    Lich attacker art

---
+++ Assets/Scripts/Character/Attacker/Lich.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Lich : MonoBehaviour
+{
+    Attacker attackerScript;
+    Health health;
+
+    void Start()
+    {
+        attackerScript = GetComponent<Attacker>();
+        health = GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        if (health.isDead)
+            this.enabled = false;
+
+        /*if (attackerScript.isAttacking == false && attackerScript.currentDefenderAttacking != null
+            && Vector2.Distance(transform.position, attackerScript.currentDefenderAttacking.transform.position) <= attackerScript.minAttackDistance)
+        {
+            // Debug.Log(name + " is attacking " + defenderScript.targetAttacker.name);
+            attackerScript.Attack();
+        }*/
+    }
+
+    public void RaiseUndead()
+    {
+
+    }
+
+    public void ShootFireball()
+    {
+        // Put this in own script? AttackerShooter?
+    }
+}
